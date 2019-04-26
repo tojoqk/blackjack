93e4922e74b5ba5f1b33700fbe49a3c7b9db7fec
Masaya Tojo <masaya@tojo.tokyo>
2019-04-27 02:39:04 +0900

    Add cards

--- blackjack.rkt
+++ blackjack.rkt
@@ -8,3 +8,21 @@
 (define-predicate rank-number? (U 2 3 4 5 6 7 8 9 10))
 (define-predicate rank-face? (U 'Jack 'Queen 'King))
 (define-predicate rank-ace? 'Ace)
+
+(struct card ([suit : Suit] [rank : Rank]))
+
+(: suits (Listof Suit))
+(define suits '(Spade Club Heart Diamond))
+
+(: ranks (Listof Rank))
+(define ranks '(Ace 2 3 4 5 6 7 8 9 10 Jack Queen King))
+
+(: product (All (A B C) (-> (-> A B C) (Listof A) (Listof B) (Listof C))))
+(define (product f xs ys)
+  (for*/list : (Listof C) ([x : A xs]
+                           [y : B ys])
+    (f x y)))
+
+(: cards (Listof card))
+(define cards
+  (product card suits ranks))
