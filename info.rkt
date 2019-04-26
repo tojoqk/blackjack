6a7cf70f8145a8e7471ff9d9710f54c9b9492b9a
Masaya Tojo <masaya@tojo.tokyo>
2019-04-27 02:07:52 +0900

    Set version 0.1

--- info.rkt
+++ info.rkt
@@ -4,5 +4,5 @@
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/blackjack.scrbl" ())))
 (define pkg-desc "Description Here")
-(define version "0.0")
+(define version "0.1")
 (define pkg-authors '(tojoqk))
