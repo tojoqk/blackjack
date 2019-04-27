9df92c503ad62855f51ebcbd22aff834f59840d3
Masaya Tojo <masaya@tojo.tokyo>
2019-04-27 17:28:15 +0900

    Add tests

--- blackjack.rkt
+++ blackjack.rkt
@@ -1,5 +1,8 @@
 #lang typed/racket
 
+(module+ test
+  (require typed/rackunit))
+
 (define-type Suit (U 'Spade 'Club 'Heart 'Diamond))
 (define-predicate suit? Suit)
 
@@ -17,12 +20,25 @@
 (: ranks (Listof Rank))
 (define ranks '(Ace 2 3 4 5 6 7 8 9 10 Jack Queen King))
 
+(module+ test
+  (check-eqv? (count rank-number? ranks) 9)
+  (check-eqv? (count rank-face? ranks) 3)
+  (check-eqv? (count rank-ace? ranks) 1))
+
 (: product (All (A B C) (-> (-> A B C) (Listof A) (Listof B) (Listof C))))
 (define (product f xs ys)
   (if (null? xs)
       '()
       (append (map (lambda ([y : B]) (f (car xs) y)) ys)
               (product f (cdr xs) ys))))
+
+(module+ test
+  (check-equal? (product (inst cons Integer Symbol)
+                         '(1 2 3)
+                         '(a b c))
+                (list '(1 . a) '(1 . b) '(1 . c)
+                      '(2 . a) '(2 . b) '(2 . c)
+                      '(3 . a) '(3 . b) '(3 . c))))
 
 (: cards (Listof card))
 (define cards
