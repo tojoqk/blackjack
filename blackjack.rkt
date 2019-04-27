8baf1b903f737913fe6e8d42461912cd0194151f
Masaya Tojo <masaya@tojo.tokyo>
2019-04-28 04:53:44 +0900

    Add tests

--- blackjack.rkt
+++ blackjack.rkt
@@ -124,6 +124,8 @@
   (check-pred win?
               (judge 15 10))
   (check-pred lose?
+              (judge 10 15))
+  (check-pred lose?
               (judge 'Bust 3))
   (check-pred win?
               (judge 2 'Bust)))
