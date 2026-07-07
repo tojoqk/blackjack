a2fa05a0b35841eab7f469360154a26ce18fd374
Masaya Tojo <masaya@tojo.tokyo>
2026-07-07 09:08:41 +0900

    Add graph-executor to deps

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "blackjack")
-(define deps '("base" "typed-racket-lib" "2d-lib"))
+(define deps '("base" "typed-racket-lib" "2d-lib" "https://github.com/tojoqk/graph-executor.git"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "rackunit-typed"))
 (define scribblings '(("scribblings/blackjack.scrbl" ())))
 (define pkg-desc "Typed BlackJack")
